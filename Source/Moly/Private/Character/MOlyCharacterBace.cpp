4c6840a6742da8a61ff312941b6253968c002a68
SADY <sady1366@gmail.com>
2024-06-25 19:28:38 +0330

    Create Animation Blueprints

--- Source/Moly/Private/Character/MOlyCharacterBace.cpp
+++ Source/Moly/Private/Character/MOlyCharacterBace.cpp
@@ -8,6 +8,9 @@ AMOlyCharacterBace::AMOlyCharacterBace()
 {
 	PrimaryActorTick.bCanEverTick = false;
 
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon-> SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon-> SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 void AMOlyCharacterBace::BeginPlay()
